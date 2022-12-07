92ed8cf1c521800526bf4cc55d5f3f3200827c91
modolet <modolet@modolet.cn>
2022-12-08 00:47:33 +0800

    使用循环加载plugins内的文件夹

--- init.lua
+++ init.lua
@@ -2,4 +2,11 @@ require('basic')
 require('keybindings')
 require('plugins')
 require('colorscheme')
-require('settings.nvim-tree')
+-- require('settings.nvim-tree')
+-- require('settings.bufferline')
+-- require('settings.lualine')
+
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins', [[v:val =~ '\.lua$']])) do
+  require('plugins.'..file:gsub('%.lua$', ''))
+end
+
