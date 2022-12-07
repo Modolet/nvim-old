d2e193b60b4f546964ac26b6e6d6ec5aa66bf446
modolet <modolet@modolet.cn>
2022-12-08 00:48:19 +0800

    优化键位

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -13,10 +13,10 @@ map("n","sh",":sp<CR>",opt) -- 纵向分屏
 map("n","sc","<C-w>c",opt) -- 关闭当前
 map("n","so","<C-w>o",opt) -- 关闭其他
 -- 跳转
-map("n","<A-h>","<C-w>h",opt)
-map("n","<A-j>","<C-w>j",opt)
-map("n","<A-k>","<C-w>k",opt)
-map("n","<A-l>","<C-w>l",opt)
+map("n","eh","<C-w>h",opt)
+map("n","ej","<C-w>j",opt)
+map("n","ek","<C-w>k",opt)
+map("n","el","<C-w>l",opt)
 -- 左右比例控制
 map("n", "<C-Left>",":vertical resize -2<CR>",opt)
 map("n", "<C-Right>",":vertical resize +2<CR>",opt)
@@ -27,13 +27,13 @@ map("n", "<C-Down>",":resize -2<CR>",opt)
 map("n","s=","<C-w>=",opt)
 
 -- Terminal相关
-map("n","th",":sp | terminal<CR>",opt)
-map("n","tv",":vsp | terminal<CR>",opt)
+map("n","<Leader>th",":sp | terminal<CR>",opt)
+map("n","<Leader>tv",":vsp | terminal<CR>",opt)
 map("t","<Esc>","<C-\\><C-n>",opt)
-map("n","<A-h>",[[ <C-\><C-N><C-w>h ]],opt)
-map("n","<A-j>",[[ <C-\><C-N><C-w>j ]],opt)
-map("n","<A-k>",[[ <C-\><C-N><C-w>k ]],opt)
-map("n","<A-l>",[[ <C-\><C-N><C-w>l ]],opt)
+map("n","eh",[[ <C-\><C-N><C-w>h ]],opt)
+map("n","ej",[[ <C-\><C-N><C-w>j ]],opt)
+map("n","ek",[[ <C-\><C-N><C-w>k ]],opt)
+map("n","el",[[ <C-\><C-N><C-w>l ]],opt)
 
 -- 代码移动
 -- 上下滚动浏览
@@ -52,10 +52,10 @@ map("v", "J", ":move '>+1<CR>gv-gv", opt)
 map("v", "K", ":move '<-2<CR>gv-gv", opt)
 
 -- 保存、退出
-map("n","Q",":q<CR>",opt)
-map("n","QQ",":q!<CR>",opt)
-map("n","QQQ",":qa!<CR>",opt)
-map("n","S",":w<CR>",opt)
+map("n","<C-q>",":q<CR>",opt)
+map("n","<C-a><C-q>",":qa<CR>",opt)
+map("n","<C-s>",":w<CR>",opt)
+map("n","<C-a><C-s>",":wa<CR>",opt)
 
 -- 插件快捷键
 local pluginKeys = {}
@@ -81,4 +81,19 @@ pluginKeys.nvimTree = {
   { key = "p", action = "paste" },
   { key = "s", action = "system_open" },
 }
+
+-- bufferline
+-- 新建
+map("n","Tn",":tabnew<CR>",opt)
+-- 左右TAB切换
+map("n","Tl",":BufferLineCycleNext<CR>",opt)
+map("n","Th",":BufferLineCyclePrev<CR>",opt)
+-- 关闭
+map("n","Tcc",":BDelete<CR>",opt) -- 关闭一个
+map("n","Tcl",":BufferLineCoseLeft<CR>",opt) -- 关闭左边
+map("n","Tcr",":BufferLineCoseRight<CR>",opt) -- 关闭右边
+map("n","Tca",":BufferLinePickClose<CR>",opt) -- 关闭所有
+
+
+
 return pluginKeys
