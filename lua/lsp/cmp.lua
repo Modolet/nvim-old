340c5b8329a173a899db9b2d6ee01f69d196c7ad
modolet <modolet@modolet.cn>
2022-12-09 21:06:24 +0800

    设置中文也能触发不全

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -3,3 +3,14 @@ if not status then
 	vim.notify("未找到 cmp")
 	return
 end
+cmp.setup({
+	-- ghost text
+	experimental = {
+		ghost_text = true,
+	},
+	-- 设置中文也能触发
+	completion = {
+		-- autocomplete = false,
+		-- keyword_length = 1,
+		keyword_pattern = "[\\u4e00-\\u9fa5a-zA-Z^0-9]",
+	},
