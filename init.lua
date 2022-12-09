5aa3514738a506ef5342050d28849452a4443db5
modolet <modolet@modolet.cn>
2022-12-09 21:11:56 +0800

    add:复制时高亮、WSL复制到系统剪切板

--- init.lua
+++ init.lua
@@ -1,12 +1,33 @@
-require('basic')
-require('keybindings')
-require('plugins')
-require('colorscheme')
--- require('settings.nvim-tree')
--- require('settings.bufferline')
--- require('settings.lualine')
+require("basic")
+require("keybindings")
+require("plugins")
+require("colorscheme")
+require("plugins.nvim-notify")
 
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins', [[v:val =~ '\.lua$']])) do
-  require('plugins.'..file:gsub('%.lua$', ''))
+-- 复制到系统剪切板
+vim.cmd([[
+augroup fix_yank
+    autocmd!
+    autocmd TextYankPost * if v:event.operator ==# 'y' | call system('/mnt/c/Windows/System32/clip.exe', @0) | endif
+augroup END
+]])
+
+-- 复制时高亮
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
+	end,
+})
+
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/plugins", [[v:val =~ '\.lua$']])) do
+	require("plugins." .. file:gsub("%.lua$", ""))
 end
 
+require("lsp.setup")
+require("lsp.cmp")
+require("lsp.ui")
+
+vim.notify([[欢迎使用Modolet的NVIM，祝您愉快~😀]])
