3f2a3680a13363ec10be6c35338e0e04aa30f230
modolet <modolet@modolet.cn>
2022-12-16 03:49:22 +0800

    换用toggleterm

--- lua/plugins/Fterm.lua
+++ lua/plugins/Fterm.lua
@@ -1,15 +1,15 @@
-local status ,fterm = pcall(require,"FTerm")
-if not status then
-  vim.notify("未找到 FTerm")
-  return
-end
-
-fterm.setup({
-  border = "double",
-  dimensions = {
-    height = 0.9,
-    width = 0.9,
-  },
-  auto_close = true,
-})
-
+-- local status ,fterm = pcall(require,"FTerm")
+-- if not status then
+--   vim.notify("未找到 FTerm")
+--   return
+-- end
+--
+-- fterm.setup({
+--   border = "double",
+--   dimensions = {
+--     height = 0.9,
+--     width = 0.9,
+--   },
+--   auto_close = true,
+-- })
+--
