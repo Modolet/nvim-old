62c7dcce811d68cdc8eebf1ff2229ae37bcba21d
modolet <modolet@modolet.cn>
2022-12-16 03:50:03 +0800

    添加indent-blackline配置

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+require("indent_blankline").setup({
+	-- for example, context is off by default, use this to turn it on
+	show_current_context = true,
+	show_current_context_start = true,
+	use_treesitter = true,
+	filetype_exclude = {
+		"lspinfo",
+		"packer",
+		"checkhealth",
+		"help",
+		"main",
+		"dashboard",
+		"nvim",
+		"NvimTree",
+	},
+})
