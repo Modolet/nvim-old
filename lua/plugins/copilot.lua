8367d448583453403314bfdf72200cee7be374fe
modolet <modolet@modolet.cn>
2022-12-16 03:49:38 +0800

    设置代理

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,9 @@
--- vim.g.copilot_proxy = "172.27.208.1:6789"
+-- 获取环境变量中的host_ip
+local host_ip = os.getenv("host_ip")
+vim.g.copilot_proxy = host_ip .. ":6789"
+require("copilot_cmp").setup({
+    method = "getPanelCompletions",
+    formatters = {
+        insert_text = require("copilot_cmp.format").remove_existing,
+    },
+})
